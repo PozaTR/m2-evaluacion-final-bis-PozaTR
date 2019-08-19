e52b89aea7e57c02f81c365f07d24ffdc5fba02c
Rocio Poza <rocio.poza.tejeda@gmail.com>
2019-08-19 16:46:55 +0200

    get input value to the form

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
-console.log('>> Ready :)');
+const choiceForm = document.querySelector('.js__pairs__choice');
+
+function pokemonChoice(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const valueRadio = formData.get('pokemon-choice');
+}
+
+choiceForm.addEventListener('submit', pokemonChoice);
